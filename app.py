12b0b5961b1d269ced375518974f13307ac2a1e2
gladys-pascual <gladyskate@gmail.com>
2021-05-25 18:37:10 +0100

    Add Log In functionality.

--- app.py
+++ app.py
@@ -30,11 +30,11 @@ def get_games():
 def register():
     if request.method == "POST":
         # Check if username already exists in the database
-        existing_user = mongo.db.useres.find_one(
+        existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
         if existing_user:
             flash("Username already exists, please try a new one.")
-            return redirect(url_for("register.html"))
+            return redirect(url_for("register"))
 
         register = {
             "username": request.form.get("username").lower(),
@@ -45,8 +45,35 @@ def register():
         # Put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration was successful!")
+        return render_template("login.html")
+
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if username exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # Invalid passordmatch
+                flash("Invalid username and/or password.")
+                return redirect(url_for("login"))
+
+        else:
+            # Username doesn't exist
+            flash("Incorrect username and/or password.")
+            return redirect(url_for("login"))
+    
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
