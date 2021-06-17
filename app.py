ce4edd42cbdd75ea1d027ee3fd4a8d3e9be30e3c
gladys-pascual <gladyskate@gmail.com>
2021-06-17 23:48:42 +0100

    Add errorhandler

--- app.py
+++ app.py
@@ -377,6 +377,18 @@ def edit_player(player_id):
     return render_template("edit_player.html", player=player)
 
 
+# 404 Error Page
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html", error=error), 404
+
+
+# 500 Error Page
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("500.html", error=error), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
